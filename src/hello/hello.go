2e891e2951615447d5b424198ce258cb852f5383
qiuding <880807>
2016-10-09 22:21:51 +0800

    update go hello and stringutols

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"../stringutils"
+)
+
 /**
 my first program in go !
  */
-func main()  {
+func main() {
 	fmt.Println("horld world !")
 	fmt.Println("hello go!")
 
@@ -12,26 +16,26 @@ func main()  {
 	var f float32;
 	i = 8
 	f = 7
-	fmt.Println("i == ",i)
-	fmt.Println("f == ",f)
-	fmt.Printf("i's type is %T\n",i)
-	fmt.Printf("f's type is %T\n",f)
+	fmt.Println("i == ", i)
+	fmt.Println("f == ", f)
+	fmt.Printf("i's type is %T\n", i)
+	fmt.Printf("f's type is %T\n", f)
 
-	var a,b = 1,"foo"
-	fmt.Println("a == ",a,"b == ",b)
-	fmt.Printf("a's type is %T\n",a)
-	fmt.Printf("b's type is %T\n",b)
+	var a, b = 1, "foo"
+	fmt.Println("a == ", a, "b == ", b)
+	fmt.Printf("a's type is %T\n", a)
+	fmt.Printf("b's type is %T\n", b)
 
 	var ptr *int
 	ptr = &i
 	fmt.Println("ptr == ", ptr)
-	fmt.Println("ptr == ", *ptr)
+	fmt.Println("*ptr == ", *ptr)
 
 	var x interface{}
 
 	switch i := x.(type) {
 	case nil:
-		fmt.Printf("type of x :%T",i)
+		fmt.Println("type of x :%T", i)
 	case int:
 		fmt.Printf("x is int")
 	case float64:
@@ -43,6 +47,9 @@ func main()  {
 	default:
 		fmt.Printf("don't know the type")
 	}
+
+	fmt.Println(stringutils.Reverse("!oG ,olleH"));
+
 
 
 }
